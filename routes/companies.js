c876ff81dc9d34adcc17682fcf3eef66eca91b3e
Kamille Magdirila <kamillenmagdirila@gmail.com>
2024-05-20 20:46:17 -0700

    further study

--- routes/companies.js
+++ routes/companies.js
@@ -1,3 +1,6 @@
+/** Routes for companies. */
+
+
 const express = require("express");
 const slugify = require("slugify");
 const ExpressError = require("../expressError")
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,3 +1,6 @@
+/** Routes for invoices. */
+
+
 const express = require("express");
 const ExpressError = require("../expressError")
 const db = require("../db");
